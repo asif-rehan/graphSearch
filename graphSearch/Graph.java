ddd35707537b2e676f3034c2ca7f25fee1e9da2b
rey832 <asif.rehan9@gmail.com>
2015-08-31 16:02:42 -0700

    nodeList stores node coords in a vertical column with size numVerticesX2

--- graphSearch/Graph.java
+++ graphSearch/Graph.java
@@ -7,10 +7,12 @@ public class Graph {
 	private int numEdges;
 	private int numVertices;
 	private Vector<TreeMap<Integer, Integer>> adjList;
+	private double[][] nodeList;
 	public Graph(int n) {
 		numEdges = 0;
 		numVertices = n;
 		adjList = new Vector<TreeMap<Integer,Integer>>();
+		nodeList = new double[numVertices][2]; //x,y for each node
 		for (int i = 0; i < numVertices; i++) {
 			adjList.add(new TreeMap<Integer, Integer>());
 		}
@@ -48,5 +50,9 @@ public class Graph {
 	}
 	public TreeMap<Integer, Integer> getAdjList(Integer v){
 		return adjList.get(v);
+	}
+	public void addNodeXY(Integer NodeID, double nodeX, double nodeY){
+		nodeList[NodeID][0] = nodeX;
+		nodeList[NodeID][1] = nodeY;
 	}
 }
--- graphSearch/Path.java
+++ graphSearch/Path.java
@@ -21,4 +21,3 @@ public class Path {
 		return pathEdges;
 	}
 }
-
